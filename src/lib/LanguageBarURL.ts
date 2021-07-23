53653a979b972e5c43c20504c4179025b84f1486
Vincent <thisoneis4business@gmail.com>
2021-07-24 01:56:15 +0700

    🏳️ Expanded language bar choices

--- src/lib/LanguageBarURL.ts
+++ src/lib/LanguageBarURL.ts
@@ -6,4 +6,4 @@
 //  Copyright © 2021 d-exclaimation. All rights reserved.
 //
 
-export const langBarOptions: Set<string> = new Set(['exclaim', 'typescript', 'rust', 'swift', 'javascript', 'go', 'c#', 'elixir', 'python', 'c++', 'kotlin', 'shell']);
+export const langBarOptions: Set<string> = new Set(['exclaim', 'typescript', 'rust', 'swift', 'javascript', 'go', 'c#', 'elixir', 'python', 'c++', 'kotlin', 'shell', 'scala', 'haskell', 'erlang', 'sql']);
